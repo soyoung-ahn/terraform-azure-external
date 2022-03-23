6428c7a8810dc51d8a9bcedb299fd5e7c336a362
hyeinee3 <86913926+hyeinee3@users.noreply.github.com>
2022-03-23 16:41:14 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/22"]
   location            = "koreacentral"
-  resource_group_name = azurerm_resource_group.main.name
+  resource_group_name = "DevOps-ResourceGroup"
 }
 
 resource "azurerm_subnet" "internal" {
