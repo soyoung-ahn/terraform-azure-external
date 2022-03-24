6ce00209714c545dabe0ccc7d65a0c63bc64ae24
hyeinee3 <86913926+hyeinee3@users.noreply.github.com>
2022-03-24 13:37:24 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,21 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
+  prefix = "terraform-azure-external-HI"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
+  location = "koreacentral"
 }
 
 variable "username" {
   description = "admin username"
+  username = "devos"
 }
 
 variable "password" {
   description = "admin password"
+  password = "Pa55w0rd"
 }
 
 
